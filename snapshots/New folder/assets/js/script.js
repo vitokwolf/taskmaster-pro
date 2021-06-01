3bf697320f53adcf9ba15333968d9b4aba2d87fa
Victor Lupascu <sinajeen@gmail.com>
2021-06-01 13:50:43 -0500

    style using bootstrap

--- snapshots/New folder/assets/js/script.js
+++ snapshots/New folder/assets/js/script.js
@@ -1,6 +1,6 @@
 var tasks = {};
 
-var createTask = function (taskText, taskDate, taskList) {
+var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
   var taskSpan = $("<span>")
@@ -18,7 +18,7 @@ var createTask = function (taskText, taskDate, taskList) {
   $("#list-" + taskList).append(taskLi);
 };
 
-var loadTasks = function () {
+var loadTasks = function() {
   tasks = JSON.parse(localStorage.getItem("tasks"));
 
   // if nothing in localStorage, create a new object to track all task status arrays
@@ -32,33 +32,36 @@ var loadTasks = function () {
   }
 
   // loop over object properties
-  $.each(tasks, function (list, arr) {
-    // console.log(list, arr);
+  $.each(tasks, function(list, arr) {
+    console.log(list, arr);
     // then loop over sub-array
-    arr.forEach(function (task) {
+    arr.forEach(function(task) {
       createTask(task.text, task.date, list);
     });
   });
 };
 
-var saveTasks = function () {
+var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+
+
+
 // modal was triggered
-$("#task-form-modal").on("show.bs.modal", function () {
+$("#task-form-modal").on("show.bs.modal", function() {
   // clear values
   $("#modalTaskDescription, #modalDueDate").val("");
 });
 
 // modal is fully visible
-$("#task-form-modal").on("shown.bs.modal", function () {
+$("#task-form-modal").on("shown.bs.modal", function() {
   // highlight textarea
   $("#modalTaskDescription").trigger("focus");
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-primary").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -79,93 +82,92 @@ $("#task-form-modal .btn-primary").click(function () {
   }
 });
 
-// on click edit task description
-$('.list-group').on('click', 'p', function () {
+// task text was clicked
+$(".list-group").on("click", "p", function() {
+  // get current text of p element
   var text = $(this)
     .text()
     .trim();
-  // console.log(text);
-  var textInput = $('<textarea>')
-    .addClass('form-control')
-    .val(text);
+
+  // replace p element with a new textarea
+  var textInput = $("<textarea>").addClass("form-control").val(text);
   $(this).replaceWith(textInput);
-  textInput.trigger('focus');
+
+  // auto focus new element
+  textInput.trigger("focus");
 });
 
-// on click anywhere else save the updated task
-$('.list-group').on('blur', 'textarea', function () {
-  // get the textarea's current value/text
-  var text = $(this)
-    .val();
-    console.log(text);
-    // .trim();
-  // get the parent ul's id attribute
+// editable field was un-focused
+$(".list-group").on("blur", "textarea", function() {
+  // get current value of textarea
+  var text = $(this).val();
+
+  // get status type and position in the list
   var status = $(this)
-    .closest('.list-group')
-    .attr('id')
-    .replace('list-', '');
-    console.log(status);
-  // get the task's position in the list of other li elements
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
   var index = $(this)
-    .closest('.list-group-item')
+    .closest(".list-group-item")
     .index();
-    console.log(index);
+
+  // update task in array and re-save to localstorage
   tasks[status][index].text = text;
   saveTasks();
+
   // recreate p element
-  var taskP = $('<p>')
-    .addClass('m-1')
+  var taskP = $("<p>")
+    .addClass("m-1")
     .text(text);
-  // replace textarea with p element
+
+  // replace textarea with new content
   $(this).replaceWith(taskP);
 });
 
 // due date was clicked
-$('.list-group').on('click', 'span', function () {
-  // get current task
+$(".list-group").on("click", "span", function() {
+  // get current text
   var date = $(this)
     .text()
     .trim();
-  // create new input element 
-  var dateInput = $('<input>')
-    .attr('type', 'text')
-    .addClass('form-control')
+
+  // create new input element
+  var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
     .val(date);
-  // swap out elements
   $(this).replaceWith(dateInput);
-  // automatically focus on new elemnt
-  dateInput.trigger('focus');
+
+  // automatically bring up the calendar
+  dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$('.list-group').on('blur', 'input[type="text"]', function () {
-  // get the current text
-  var date = $(this)
-    .val();
-    // .trim();
-  // get the parent ul's id attribute 
+$(".list-group").on("blur", "input[type='text']", function() {
+  var date = $(this).val();
+
+  // get status type and position in the list
   var status = $(this)
-    .closest('.list-group')
-    .attr('id')
-    .replace('list-', '');
-  // get the task's position in the list of other li elements
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
   var index = $(this)
-    .closest('.list-group-item')
+    .closest(".list-group-item")
     .index();
-  // update task in array and re-save to localStorage
+
+  // update task in array and re-save to localstorage
   tasks[status][index].date = date;
   saveTasks();
-  // recreate span element with bootstrap classes
-  var taskSpan = $('<span>')
-    .addClass('badge badge-primary badge-pill')
+
+  // recreate span and insert in place of input element
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
     .text(date);
-  // replace input with span elemnt
-  $(this)
-    .replaceWith(taskSpan);
+    $(this).replaceWith(taskSpan);
 });
 
 // remove all tasks
-$("#remove-tasks").on("click", function () {
+$("#remove-tasks").on("click", function() {
   for (var key in tasks) {
     tasks[key].length = 0;
     $("#list-" + key).empty();
